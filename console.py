f8037a725f1493744d7476b3822233347b21e3c7
Walaafadol <walaafadul7@gmail.com>
2024-01-13 14:01:18 -0800

    task

--- console.py
+++ console.py
@@ -14,7 +14,11 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, line):
         """"Inbuilt EOF command """
-        print()
+        print("")
+        return True
+    def do_quit(self, arg):
+        """Quit command to exit the program.
+        """
         return True
 
 if __name__ == "__main__":
