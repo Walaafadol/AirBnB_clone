c3c6dc79591fc0a32ed2fe95ec66ddc964cf1bc5
Walaafadol <walaafadul7@gmail.com>
2024-01-14 08:15:07 -0800

    t

--- console.py
+++ console.py
@@ -41,8 +41,74 @@ class HBNBCommand(cmd.Cmd):
         elif len(commands) < 2:
             print("** instance id missing **")
         else:
-            object = storage.all()
-            pass
+            objects = storage.all()
+            key = "{}.{}".format(commands[0], commands[1])
+            if key in objects:
+                print(objects[key])
+            else:
+                print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """Delete a class instance of a given id."""
+        commands = shlex.split(arg)
+        if len(commands) == 0:
+            print("** class name missing **")
+        elif commands[0] not in self._classes:
+            print("** class doesn't exist **")
+        elif len(commands) < 2:
+            print("** instance id missing **")
+        else:
+            objects = storage.all()
+            key = "{}.{}".format(commands[0], commands[1])
+            if key in objects:
+                del objects[key]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        """ print representation of all instance"""
+        objects = storage.all()
+        commands = shlex.split(arg)
+        if len(commands) == 0:
+            for key, value in objects.items():
+                print(str(value))
+        elif command[0] not in self._classes:
+                print("** class doesn't exist **")
+        else:
+            for key, value in objects.items():
+                if key.split('.')[0] == commands[0]:
+                        print(str(value))
+
+    def do_update(self, arg):
+        """update an istance"""
+        commands = shlex.split(arg)
+        if len(commands) == 0:
+            print("** class name missing **")
+        elif commands[0] not in self._classes:
+            print("** class doesn't exist **")
+        elif len(commands) > 2:
+            print("** instance id missing **")
+        else:
+            objects = storage.all()
+            key = "{}.{}".format(commands[0], commands[1])
+
+            if key not in objects:
+                print("** no instance found **")
+            elif len(commands) < 3:
+                print("** attribute name missing **")
+            elif len(commands) < 4:
+                print("** value missing **")
+            else:
+                obje = objects[key]
+                attrname = commands[2]
+                attvalue = commands[3]
+                try:
+                    attvalue = eval(attvalue)
+                except Exception:
+                    pass
+                setattr(obje, attrname, attvalue)
+                obje.save()
 
 
 
